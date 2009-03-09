540c72e1fdd877278b3b61a72306bb6baf47a39d
Chris Bilson <cbilson@pobox.com>
2009-03-09 05:24:33 -0700

    added an Each extension

---
+++ Specs/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Specs {
+    public static class Extensions {
+
+        public static void Each<T>(this ICollection<T> things, Action<T, int> action) {
+            for (var i = 0; i < things.Count; i++)
+                action(things.ElementAt(i), i);
+        }
+    }
+}
